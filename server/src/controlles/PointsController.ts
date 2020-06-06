9d6a03b68a058c648b9089a3e1e8a64aab6a0bb2
Mailson <mailsonfcsilva@gmail.com>
2020-06-06 18:21:22 -0300

    inserindo campo de upload de imagem

--- server/src/controlles/PointsController.ts
+++ server/src/controlles/PointsController.ts
@@ -17,7 +17,14 @@ class PointsController {
       .distinct()
       .select("points.*");
 
-    return response.json(points);
+    const serializedPoints = points.map((point) => {
+      return {
+        ...point,
+        image_url: `http://192.168.100.6:3333/uploads/${point.image}`,
+      };
+    });
+
+    return response.json(serializedPoints);
   }
 
   async show(request: Request, response: Response) {
@@ -34,7 +41,12 @@ class PointsController {
       .where("point_items.point_id", id)
       .select("items.title");
 
-    return response.json({ point, items });
+    const serializedPoints = {
+      ...point,
+      image_url: `http://192.168.100.6:3333/uploads/${point.image}`,
+    };
+
+    return response.json({ point: serializedPoints, items });
   }
 
   async create(request: Request, response: Response) {
@@ -52,8 +64,7 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image:
-        "https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+      image: request.file.filename,
       name,
       email,
       whatsapp,
@@ -66,12 +77,15 @@ class PointsController {
 
     const point_id = insertedId[0];
 
-    const point_items = items.map((item_id: number) => {
-      return {
-        item_id,
-        point_id,
-      };
-    });
+    const point_items = items
+      .split(",")
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
+        return {
+          item_id,
+          point_id,
+        };
+      });
 
     await trx("point_items").insert(point_items);
 
